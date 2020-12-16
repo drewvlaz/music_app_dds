e0e5c6f639dbaf5d482665e37441d777b40d0704
Drew Vlasnik <314dsv@gmail.com>
2020-12-16 15:16:03 -0500

    Set up authorization using client credentials to get tokens after expiration

--- mood.py
+++ mood.py
@@ -1,5 +1,7 @@
-from secrets import SPOTIFY_TOKEN
+from secrets import CLIENT_ID, CLIENT_SECRET
 import requests
+import json
+import base64
 
 class Playlist:
     """ Contains and controls playlist elements """
@@ -7,14 +9,37 @@ class Playlist:
     def __init__(self, name):
         self.name = name
 
+    def get_Auth_Token(self):
+        """ Get authorization token from client credentials """
+
+        url = 'https://accounts.spotify.com/api/token'
+
+        # Encode to base 64
+        # Refer to https://dev.to/mxdws/using-python-with-the-spotify-api-1d02
+        message = f"{CLIENT_ID}:{CLIENT_SECRET}"
+        messageBytes = message.encode('ascii')
+        base64Bytes = base64.b64encode(messageBytes)
+        base64Message = base64Bytes.decode('ascii')
+
+        r = requests.post(
+            url,
+            headers={
+                'Authorization': f'Basic {base64Message}'
+            },
+            data={
+                'grant_type': 'client_credentials'
+            }
+        )
+
+        return r.json()['access_token']
+
     def get_recommendations(self):
         """ Get recommended songs from spotify """
-        query = f'https://api.spotify.com/v1/recommendations'
-        # query = f'https://api.spotify.com/v1/recommendations?market=US&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_tracks=0c6xIDDpzE81m2q797ordA&min_danceability=.1&max_danceability=.9&target_danceability=.4&min_energy=0.4&min_popularity=50'
-        # NOTE: Auth token expires every hour
-        # TODO: Set up auth workflow to get around this
+
+        url = f'https://api.spotify.com/v1/recommendations'
+
         self.data = requests.get(
-            query,
+            url,
             params={
                 # 'fields': 'items(track(album(artists, images), name))'
                 'market': 'US',
@@ -22,25 +47,25 @@ class Playlist:
                 'seed_genres': 'rap',
                 'seed_tracks': '0c6xIDDpzE81m2q797ordA',
                 'limit': '1'
-                # 'min_acousticness': 0.1
-                # 'max_acousticness': 0.1
-                # 'target_acousticness': 0.1
-                # 'min_dancebility': 0.1
-                # 'max_dancebility': 0.1
-                # 'target_dancebility': 0.1
-                # 'min_energy': 0.1
-                # 'max_energy': 0.1
-                # 'target_energy': 0.1
-                # 'min_liveness': 0.1
-                # 'max_liveness': 0.1
+                # 'min_acousticness': 0.1,
+                # 'max_acousticness': 0.1,
+                # 'target_acousticness': 0.1,
+                # 'min_dancebility': 0.1,
+                # 'max_dancebility': 0.1,
+                # 'target_dancebility': 0.1,
+                # 'min_energy': 0.1,
+                # 'max_energy': 0.1,
+                # 'target_energy': 0.1,
+                # 'min_liveness': 0.1,
+                # 'max_liveness': 0.1,
                 # 'target_liveness': 0.1
             },
             headers={
                 'Content-Type': 'application/json',
-                'Authorization': f'Bearer {SPOTIFY_TOKEN}',
+                'Authorization': f'Bearer {self.get_Auth_Token()}',
             }
         )
 
 pl = Playlist("Test")
 pl.get_recommendations()
-print(pl.data.json())
+print(json.dumps(pl.data.json(), indent=2))
