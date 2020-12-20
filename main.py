a8f7b994e3cb6f2561923f832b8b93b214f5d2b0
kovarikdjk <60350719+kovarikdjk@users.noreply.github.com>
2020-12-20 15:11:26 -0500

    method to write artist and genre to the folder for every state. uses cities.txt file. some files are editted since the state is so small, others are not and have cities such as Paris, Texas that will cause errors. I need to go through manually and double check each state doesnt have anything like that. I also want to add a function to search for artists from the entire state, as not every artist on the state wiki page is covered through the sum of the city pages. I will do this later without the cities.txt file.

--- main.py
+++ main.py
@@ -32,45 +32,77 @@ import requests
 from bs4 import BeautifulSoup
 from find_artists import FindArtists_wiki
 from artist_genre import ArtistGenre
+from spotify_client import SpotifyClient
 import time
-#state_names = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
 
+"""
+# overload api test
+artists = ["Tyler, The Creator", "Kendrick Lamar", "Tame Impala", "Weyes Blood", "ASAP Rocky", "Backseat Lovers", "2Pac", "James Blake", "Fionna Apple", "Billie Elish", "Arianda Grande", "Brockhampton", "the black keys", "the arcs"]
+class2 = SpotifyClient("DJ?")
 
-
-#>
-
-response = requests.get(
-    url=("https://www.britannica.com/topic/list-of-cities-and-towns-in-the-United-States-2023068"),
-)
-soup = BeautifulSoup(response.content, 'html.parser')
-soup = soup.find(class_ = "topic-content col-sm pr-lg-60").parent.find_next_sibling()
-
-x = 326620
-while (x<=326669):
-    temp_soup = soup.find(id = ("ref" + str(x)))
-    target_state = temp_soup.find(class_="h1").text.strip()
-    cities = temp_soup.find(class_="topic-list").find_all("a")
-    for a in range(len(cities)):
-        cities[a] = cities[a].text.strip()
-        path = os.getcwd() + "\Artist and Genre\\" + target_state
-        try:
-            start = time.time()
-            artists = set()
-            class1 = FindArtists_wiki(cities[a], target_state)
-            artists = class1.findArtistsByLocation()
-
-            class2 = ArtistGenre(artists)
-            genre_info = (class2.getAllArtistsGenre())
-            """for artist_x in genre_info:
-                # write line to the file with name of the city"""
-
-
-            end = time.time()
-            # prints when city is done to console
-            print(cities[a] + ", " + target_state + ":   Genre Search Time: " + str(end - start) + " for " + str(len(artists)) + " results")
-        except:
-            print(cities[a] + ", " + target_state + " does not have a page")
-            pass
-
+x=0
+a=0
+while a == 0:
+    id = class2.search_item(artists[x%len(artists)], "artist")
+    genre_info = (class2.get_artist(id))
     x += 1
+    print (genre_info)
+"""
+
+
+
+#state_names = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+state_names = ["Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+#notes: Florence, AL
+path = os.getcwd() + "\\Artist and Genre\\"
+print (path)
+
+for state in state_names:
+    cities = []
+    filename = (path + state + r"\cities.txt")
+    with open(filename, 'r') as fd:
+        for line in fd:
+            cities.append(line.rstrip("\n"))
+
+    main_path = path + state
+    for a in cities:
+        start = time.time()
+        artists = set()
+        try:
+            class1 = FindArtists_wiki(a, state)
+            artists = class1.findArtistsByLocation()
+        except:
+            print(a + ", " + state + " does not have a page")
+            continue
+
+        try_again = True
+        while try_again == True:
+            try:
+                class2 = ArtistGenre(artists)
+                artist_genre_info = (class2.getAllArtistsGenre())
+                try_again = False
+            except:
+                try_again = True
+                print (a + ":   ArtistGenre Class Error. Trying again in 2 seconds")
+                time.sleep(2)
+
+        file1 = open((main_path + "\\" + a + ".txt"), "w")
+        for ii in artist_genre_info:
+            try:
+                file1.write(ii + "\n")
+            except UnicodeEncodeError:
+                pass
+        file1.close()
+
+        end = time.time()
+
+        # prints when city is done to console
+        print(a + ", " + state + ":   Genre Search Time: " + str(end - start) + " for " + str(
+            len(artists)) + " results")
+
+    print ("COMPLETED: " + state)
+
+
+
+
 
